93918c8294bd6e1199813162742adb6e1ba6ff1b
shelcia.rosso <shelcia.rosso@thomsonreuters.com>
2021-04-05 20:25:56 -0300

    Sets: equals e hashCodes

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,14 +1,13 @@
 package br.com.alura;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
 
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new ArrayList<Aula>();
+    private Set<Aluno> alunos =  new HashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
@@ -44,5 +43,17 @@ public class Curso {
     @Override
     public String toString() {
         return "[Curso: nome " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.aulas + "]";
+    }
+
+    public void matricula(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
+
+    public boolean estaMatriculado(Aluno aluno) {
+        return this.alunos.contains(aluno);
     }
 }
