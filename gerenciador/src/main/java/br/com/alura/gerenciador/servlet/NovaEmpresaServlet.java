f9f2fa3cc3e6774659ab5d36a61faf243ef74cca
shelcia.rosso <shelcia.rosso@thomsonreuters.com>
2021-05-10 23:32:22 -0300

    JSP

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,10 +3,12 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class NovaEmpresaServlet
@@ -16,11 +18,19 @@ public class NovaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
     
-	protected void service(HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
-		String nomdeEmpresa = request.getParameter("nome");
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " + nomdeEmpresa + " cadastrada com sucesso!<body><html>");
+		String nomeEmpresa = request.getParameter("nome");
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 
 }
