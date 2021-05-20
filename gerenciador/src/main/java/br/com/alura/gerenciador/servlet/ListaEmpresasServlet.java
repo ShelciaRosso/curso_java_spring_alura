25e3d3bfde9e37d7a8cb8609e6fe5cfd746a7e17
Shelcia <shelrosso@gmail.com>
2021-05-20 12:38:52 -0300

    JPST

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -22,17 +23,10 @@ public class ListaEmpresasServlet extends HttpServlet {
 	    Banco banco = new Banco();
 	    List<Empresa> lista = banco.getEmpresas();
 	    
-	    PrintWriter out = response.getWriter();
+	    request.setAttribute("empresas", lista);
 	    
-	    out.println("<html><body>");
-	    out.println("<ul>");
-	    
-	    for (Empresa empresa : lista) {
-	    	out.println("<li>" + empresa.getNome() + "</li>");
-		}
-	    
-	    out.println("</ul>");
-	    out.println("</body></html>");
+	    RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+	    rd.forward(request, response);
 	}
 
 }
--- gerenciador/src/main/webapp/novaEmpresaCriada.jsp
+++ gerenciador/src/main/webapp/novaEmpresaCriada.jsp
@@ -1,9 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
-<%
-	String nomeEmpresa = (String)request.getAttribute("empresa");
-	System.out.println(nomeEmpresa);
-%>
-
-<html><body>
-Empresa <%= nomeEmpresa %> cadastrada com sucesso!
-</html></body>
+<html>
+	<body>
+	
+		<c:if test="${not empty empresa}">
+			Empresa ${empresa} cadastrada com sucesso!
+		</c:if>
+		
+		<c:if test="${empty empresa}">
+			Nenhuma empresa cadastrada!
+		</c:if>
+	</body>
+</html>
