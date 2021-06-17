e05a609a7f0761b51e5e037bb3b0847590e6a175
Rosso <shelcia.rosso@thomsonreuters.com>
2021-06-16 22:45:45 -0300

    Consultas com JPA

--- loja_jpa/src/main/java/br/com/alura/loja_jpa/modelo/Produto.java
+++ loja_jpa/src/main/java/br/com/alura/loja_jpa/modelo/Produto.java
@@ -10,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -25,8 +26,11 @@ public class Produto {
 	private BigDecimal preco;
 	private LocalDate dataCadastro = LocalDate.now();
 	
-	@Enumerated(EnumType.STRING)
+	@ManyToOne
 	private Categoria categoria;
+	
+	public Produto() {
+	}
 	
 	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
 		this.nome = nome;
--- loja_jpa/src/main/java/br/com/alura/loja_jpa/testes/CadastroDeProduto.java
+++ loja_jpa/src/main/java/br/com/alura/loja_jpa/testes/CadastroDeProduto.java
@@ -1,11 +1,13 @@
 package br.com.alura.loja_jpa.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja_jpa.dao.CategoriaDAO;
 import br.com.alura.loja_jpa.dao.ProdutoDAO;
 import br.com.alura.loja_jpa.modelo.Categoria;
 import br.com.alura.loja_jpa.modelo.Produto;
@@ -14,14 +16,48 @@ import br.com.alura.loja_jpa.util.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaimi Redmi", "muito legal", new BigDecimal("800"), Categoria.CELULARES);
+		cadastrarProduto();
 		
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
 		
-		ProdutoDAO dao = new ProdutoDAO(em);
+		Produto p = produtoDAO.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		//List<Produto> todos = produtoDAO.buscarTodos();
+		//List<Produto> todos = produtoDAO.buscarPorNome("Xiaimi Redmi");
+		List<Produto> todos = produtoDAO.buscarPorNomeDaCategoria("123");
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+		
+		BigDecimal preco = produtoDAO.buscarPrecoDoProdutoComNome("Xiaimi Redmi");
+		System.out.println(preco);
+	}
+
+	private static void cadastrarProduto() {
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		Categoria celulares = new Categoria("CELULARES");
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		
+		Produto celular = new Produto("Xiaimi Redmi", "muito legal", new BigDecimal("800"), celulares);
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		
+		categoriaDAO.cadastrar(celulares);
+		produtoDAO.cadastrar(celular);
+		
+		em.flush();
+		em.clear();
+		
+		celular = em.merge(celular);
+		celulares = em.merge(celulares);
+		celulares.setNome("123");
+		em.flush();
+		
+		//em.remove(celular);
+		//em.remove(celulares);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
