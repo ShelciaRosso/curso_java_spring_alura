6b970fffb986718b729d0fe492b2f71beca9e97d
Rosso <shelcia.rosso@thomsonreuters.com>
2021-09-29 21:42:36 -0300

    Spring Boot Security

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$PaT9V6PFbwMlh1gW..pnTOCgK7dgSUwL/6/akzCaENFCG3gdaCqJy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
